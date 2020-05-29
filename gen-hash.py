ff3353e5d5f7c96ed5ffecfaac2793f5be4360ef
Andrzej Sydor <andrzejsydor@gmail.com>
2020-05-29 18:02:50 +0200

    gen sha256 python script

--- gen-hash.py
+++ gen-hash.py
@@ -3,6 +3,7 @@ import os
 
 sha256 = hashlib.sha256()
 f_out = open("out.txt", "w")
+out = "Content-Securit-Policy \"default-src"
 
 for file in os.listdir():
     f_out.write(file)
@@ -10,7 +11,16 @@ for file in os.listdir():
     with open(file,"rb") as f:
         for byte_block in iter(lambda: f.read(4096),b""):
             sha256.update(byte_block)
-        f_out.write(sha256.hexdigest())
+        hexdigest=sha256.hexdigest()
+        f_out.write("sha-256"+hexdigest)
         f_out.write("\n")
+        out=out+" 'sha256-"+hexdigest+"'"
+
+out=out+"\";"
+print(out)
+
+f_out.write("generated CSP\n")
+f_out.write(out)
 
 f_out.close()
+
